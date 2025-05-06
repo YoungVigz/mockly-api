1041064db8dad0bf5e7e476620410f84b336aabc
Young Vigz <gabrielgaleza@gmail.com>
2025-05-06 14:29:56 +0200

    feat: ready to host

--- cmd/main.go
+++ cmd/main.go
@@ -8,23 +8,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/YoungVigz/mockly-api/internal/database"
 	"github.com/YoungVigz/mockly-api/internal/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	/*
-		err := database.DatabaseInit()
+	err := database.DatabaseInit()
 
-		if err != nil {
-			log.Fatal("Unable to connect to database!")
-		}*/
+	if err != nil {
+		log.Fatal("Unable to connect to database!")
+	}
 
 	fmt.Println("Connected to database")
 
 	api := gin.Default()
 	routes.RegisterRoutes(api)
-	api.Run("0.0.0.0:8080")
+	api.Run(":8080")
 }
